87a3fb029b36ee0c3f4b1186e006ba3308a0ee92
Aru2003 <baymolda03@inbox.ru>
2023-10-22 14:37:07 +0600

    2

---
+++ Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите номер трамвайного билета (6-значное число): ");
+        string ticketNumberString = Console.ReadLine();
+
+        if (ticketNumberString.Length == 6 && int.TryParse(ticketNumberString, out int ticketNumber))
+        {
+            int firstHalf = ticketNumber / 1000; // Первые три цифры
+            int secondHalf = ticketNumber % 1000; // Последние три цифры
+
+            int sumFirstHalf = SumOfDigits(firstHalf);
+            int sumSecondHalf = SumOfDigits(secondHalf);
+
+            if (sumFirstHalf == sumSecondHalf)
+            {
+                Console.WriteLine("Этот билет счастливый!");
+            }
+            else
+            {
+                Console.WriteLine("Этот билет не счастливый.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Введите корректное 6-значное число.");
+        }
+    }
+
+    // Метод для вычисления суммы цифр в числе
+    static int SumOfDigits(int number)
+    {
+        int sum = 0;
+        while (number > 0)
+        {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
