a58e51cdfde8182238eeb127e2d6de49e7304d67
Aru2003 <baymolda03@inbox.ru>
2023-10-22 15:44:06 +0600

    10 задание решение

---
+++ Task10/Task10.cs
@@ -0,0 +1,15 @@
+﻿//18.	Дана сторона квадрата. Найти его периметр.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите длину стороны квадрата: ");
+        double sideLength = double.Parse(Console.ReadLine());
+
+        double perimeter = 4 * sideLength;
+
+        Console.WriteLine("Периметр квадрата равен: " + perimeter);
+    }
+}
