a7771812a8f88c6eddbfd9996f2e4a8713fcdf09
Aru2003 <baymolda03@inbox.ru>
2023-10-22 14:27:22 +0600

    1 ЗАДАНИЕ

--- SU.Module2/Program.cs
+++ SU.Module2/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Task1.Run();
-        Task2.Run();
-        // Вызывайте методы для остальных заданий
+        char inputChar;
+        int spaceCount = 0;
+
+        Console.WriteLine("Введите текст. Для завершения введите точку (.)");
+
+        do
+        {
+            inputChar = Console.ReadKey().KeyChar;
+            if (inputChar == ' ')
+            {
+                spaceCount++;
+            }
+        } while (inputChar != '.');
+
+        Console.WriteLine("\nКоличество пробелов: " + spaceCount);
     }
 }
-    
