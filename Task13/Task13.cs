c630b6658e1ed346d03a5f23e3ae7653df86bf39
Aru2003 <baymolda03@inbox.ru>
2023-10-22 15:54:55 +0600

    13 задание решение

---
+++ Task13/Task13.cs
@@ -0,0 +1,18 @@
+﻿//23.	Известны объем и масса тела. Определить плотность материала этого тела.
+using System;
+
+class Task13
+{
+    static void Main()
+    {
+        Console.Write("Введите массу тела (в килограммах): ");
+        double mass = double.Parse(Console.ReadLine());
+
+        Console.Write("Введите объем тела (в кубических метрах): ");
+        double volume = double.Parse(Console.ReadLine());
+
+        double density = mass / volume;
+
+        Console.WriteLine("Плотность материала тела: " + density + " килограммов на кубический метр.");
+    }
+}
