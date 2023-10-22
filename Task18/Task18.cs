571175b26498d8e823a229a5b3ef2dedb06e0dc2
Aru2003 <baymolda03@inbox.ru>
2023-10-22 16:09:56 +0600

    18

---
+++ Task18/Task18.cs
@@ -0,0 +1,28 @@
+﻿//11.Вычислить значение логического выражения при следующих значениях логических величин А, В и С: А = Истина, В = Ложь, С = Ложь:
+//a.А или В;
+//b.А и В;
+//c.В или С
+
+using System;
+
+class Task18
+{
+    static void Main()
+    {
+        bool A = true;
+        bool B = false;
+        bool C = false;
+
+        // a. А или В
+        bool resultA = A || B;
+        Console.WriteLine("Значение А или В: " + resultA);
+
+        // b. А и В
+        bool resultB = A && B;
+        Console.WriteLine("Значение А и В: " + resultB);
+
+        // c. В или С
+        bool resultC = B || C;
+        Console.WriteLine("Значение В или С: " + resultC);
+    }
+}
