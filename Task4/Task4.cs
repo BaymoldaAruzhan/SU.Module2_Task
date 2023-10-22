d32d00a60fc96df3775a54946dfefc6f2b11656c
Aru2003 <baymolda03@inbox.ru>
2023-10-22 15:14:30 +0600

    Задание 5  решение

---
+++ Task4/Task4.cs
@@ -0,0 +1,12 @@
+﻿//7.Вывести на экран числа 5, 10 и 21 одно под другим.
+using System;
+
+class Task4
+{
+    static void Main()
+    {
+        Console.WriteLine(5);
+        Console.WriteLine(10);
+        Console.WriteLine(21);
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿//5.Дано расстояние в сантиметрах. Найти число полных метров в нем.
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите расстояние в сантиметрах: ");
+        int distanceInCentimeters = int.Parse(Console.ReadLine());
+
+        int fullMeters = distanceInCentimeters / 100;
+
+        Console.WriteLine("Число полных метров: " + fullMeters);
+    }
+}
