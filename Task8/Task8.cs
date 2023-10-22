e9e3050120af133510fb628d40b3d04dda21bfdc
Aru2003 <baymolda03@inbox.ru>
2023-10-22 15:41:03 +0600

    9 задание решение

---
+++ Task8/Task8.cs
@@ -0,0 +1,15 @@
+﻿//16.	вычисления значения функции y= 7x2-3x+6 при любом значении x;
+using System;
+
+class Task8
+{
+    static void Main()
+    {
+        Console.Write("Введите значение x: ");
+        double x = double.Parse(Console.ReadLine());
+
+        double y = 7 * Math.Pow(x, 2) - 3 * x + 6;
+
+        Console.WriteLine("Значение функции y = 7x^2 - 3x + 6 при x = " + x + " равно " + y);
+    }
+}
---
+++ Task9/Task9.cs
@@ -0,0 +1,15 @@
+﻿//17.вычисления значения функции x= 12a2+7a-16 при любом значении а.
+using System;
+
+class Task8
+{
+    static void Main()
+    {
+        Console.Write("Введите значение a: ");
+        double a = double.Parse(Console.ReadLine());
+
+        double x = 12 * Math.Pow(a, 2) +7 * a - 16;
+
+        Console.WriteLine("Значение функции x= 12a2+7a-16 при a = " + a + " равно " + x);
+    }
+}
