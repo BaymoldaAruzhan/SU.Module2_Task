27a59c1e9e3488fb7cb470a6bd53b7b337b6b582
Aru2003 <baymolda03@inbox.ru>
2023-10-22 15:48:07 +0600

    11 Задание решение

---
+++ Task11/Task11.cs
@@ -0,0 +1,15 @@
+﻿//19.Дан радиус окружности. Найти ее диаметр.
+using System;
+
+class Task11
+{
+    static void Main()
+    {
+        Console.Write("Введите радиус окружности: ");
+        double radius = double.Parse(Console.ReadLine());
+
+        double diameter = 2 * radius;
+
+        Console.WriteLine("Диаметр окружности с заданным радиусом равен: " + diameter);
+    }
+}
